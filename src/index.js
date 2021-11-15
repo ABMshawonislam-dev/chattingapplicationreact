1e431b42b2a0720c7cab3dad3bf7fe28e8bad147
ABMshawonislam-dev <abmshawonislam@gmail.com>
2021-11-15 12:02:59 +0600

    redirection done after login

--- src/index.js
+++ src/index.js
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
-import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
-
+import {BrowserRouter as Router,Routes,Route,Navigate} from "react-router-dom";
+import { getAuth } from './firebase';
 import 'semantic-ui-css/semantic.min.css'
 
-const Routing = () =>(
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
+
+
+class Routing extends Component{
+  state={
+    tracker: false
+  }
+  componentDidMount(){
+    getAuth().onAuthStateChanged((user)=>{
+      if(user){
+        this.setState({tracker: true})
+      }else{
+        this.setState({tracker: false})
+      }
+    })
+  }
+  render(){
+    return(
+      <Router>
+        {this.state.tracker ? 
+          <Routes>
+            <Route path="/" element={<App />}></Route>
+            <Route path="/register" element={<Navigate to="/" />}></Route>
+            <Route path="/login" element={<Navigate to="/" />}></Route>
+        </Routes>
+          
+        
+        :
+          <Routes>
+            <Route path="/" element={<App />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+        </Routes>
+        
+        }
+      
     </Router>
-)
+    )
+  }
+}
 
 ReactDOM.render(<Routing />,document.getElementById('root'));
